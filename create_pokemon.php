6af763339a4f996fc2689cd44e1bacfce1d23942
Thibaud BARDIN <thibaud.bardin@gmail.com>
2015-04-03 12:20:56 +0200

    Update

--- create_pokemon.php
+++ create_pokemon.php
@@ -4,10 +4,3 @@ require 'bootstrap.php';
 
 // TODO Validate form
 
-var_dump(
-    createPokemon($link, 'Magicarpe', 3, 100)
-);
-
-var_dump(
-    mysqli_error($link)
-);
--- template/create_pokemon.php
+++ template/create_pokemon.php
@@ -1,5 +1,32 @@
 <?php include '_header.php'; ?>
 
-<!-- Form -->
+<form xmlns="http://www.w3.org/1999/html">
+    <div class="form-group">
+        <label for="name">Name</label>
+        <input type="text" class="form-control" id="name" name="name" placeholder="Enter a name" required autofocus>
+    </div>
+    <div class="form-group">
+        <label for="hp">Hp</label>
+        <input type="number" class="form-control" id="hp" name="hp" placeholder="Enter HP" required>
+    </div>
+    <div class="form-group">
+        <label for="level">Level</label>
+        <input type="number" class="form-control" id="level" name="level" placeholder="Enter level" required>
+    </div>
+    <div class="form-group">
+        <label for="evolution">Evolution</label>
+        <select class="form-control" id="evolution" name="evolution">
+            <option value="1">Salamèche</option>
+            <option value="2">Reptincel</option>
+            <option value="3">Dracofeu</option>
+        </select>
+    </div>
+    <div class="form-group">
+        <label for="picture">Picture</label>
+        <input type="file" id="picture" name="picture">
+        <p class="help-block">Define the pokemon's picture</p>
+    </div>
+    <input type="submit" class="btn btn-default">Create pokemon</input>
+</form>
 
 <?php include '_footer.php'; ?>
